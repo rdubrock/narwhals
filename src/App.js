8d968afaec545e290c6bd8e210e11ef64c91d6b3
Russ DuBrock <rdubrock@marketo.com>
2017-12-14 10:50:20 -0800

    adds react router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src='narwhal_alone.jpg' className="App-logo" alt="logo" />
+          <img src='jedi_narwhal.jpg' className="App-logo" alt="logo" />
           <h1 className="App-title">Narwhals, Yeah!</h1>
         </header>
         <p className="App-intro">
@@ -14,12 +14,12 @@ class App extends Component {
         </p>
         <h3>Diet</h3>
         <p>
-          It feds on bethic prey, mostly flatfish. In summer it eats cod, halibut, squid, and would definitely eat Mitch. They have no teeth, they suck in their prey and swallow it whole.
+          It feeds on bethic prey, mostly flatfish. In summer it eats cod, halibut, squid, and would definitely eat Mitch. They have no teeth, they suck in their prey and swallow it whole. They eat upto 15 times a day, they eat 25 to 50 pounds per day (like Matthew Cockcroft).
         </p>
         <h3>Where on earth is the animal found?</h3>
         <p>Canadian Artic, Greenlandic & Russian Waters</p>
         <h3>What natural surroundings does the animal need to thrive?</h3>
-        <p>Lives in the water but not in freezing waters. Also on the Serengeti, but no the frozen Serengeti</p>
+        <p>Lives in the water but not in freezing waters. Also on the Serengeti, but not the frozen Serengeti</p>
         <h3>Where does the animal make its home?</h3>
         <p>They migrate from the bays to the ocean as summer comes</p>
         <h3>Mating and reproduction</h3>
@@ -28,7 +28,7 @@ class App extends Component {
         <p> Primary Species of animal: Odontoceti </p>
         <p>Animal's scientific classification: whale in Mondontidae Family </p>
         <p>Scientific name: Monodon Monoceros</p>
-        <h3>Anatamoy and biology</h3>
+        <h3>Anatomy and biology</h3>
         <p>
           Height: 3.95-5.5m
           Weight: 800-1600kgs
@@ -43,9 +43,13 @@ class App extends Component {
         <h3>Behavior</h3>
         <p>The animal gets food by diving to the bottom of the water and eating.  Almost all modern predation of narwhals is by humans.Other predators are polar bears, which attack at breathing holes mainly for young narwhals, Greenland sharks and walruse.  Killer whales (orcas) group together to overwhelm narwhal pods in the shallow water of enclosed bays,   To escape predators such as orcas, narwhals may use prolonged submergence to hide under ice floes rather than relying on speed.  Narwhals have to consciously decide to breathe, unlike us. This means that the whales don't sleep in quite the same way that we do, since part of their brain must make the decision to surface for air.</p>
         <h3>Image gallery</h3>
+        <h4>A Lovely Narwhal</h4>
         <img src='Narwhal.jpg' alt="logo" />
+        <h4>Another Lovely Narwhal</h4>
         <img src='narwhal_pod.jpg' alt="logo" />
+        <h4>Narwhals are better than dolphins</h4>
         <img src='narwhal_tooth.jpg' alt="logo" />
+        <h4>Steve the narwhal</h4>
         <img src='narwhals_underwater.jpg' alt="logo" />
         <p>To submit your own narwhal picture, send us an email at narwahls@narwhals.narwhal</p>
       </div>
