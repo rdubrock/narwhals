3ac0879af6aa78ca46d4f4a6ed13a5e3025574ee
Russ DuBrock <rdubrock@marketo.com>
2017-12-14 16:10:23 -0800

    updated diet

--- src/Diet.js
+++ src/Diet.js
@@ -5,9 +5,9 @@ export default class Diet extends Component {
     return (
       <div className="Diet">
         <h3>Diet</h3>
-        <p>
-          It feeds on bethic prey, mostly flatfish. In summer it eats cod, halibut, squid, and would definitely eat Mitch. They have no teeth, they suck in their prey and swallow it whole. They eat upto 15 times a day, they eat 25 to 50 pounds per day (like Matthew Cockcroft).
-        </p>
+        <h1>
+          Narwhals eat griffins. Exclusively.
+        </h1>
       </div>
     );
   }
