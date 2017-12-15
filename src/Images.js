be3779a9b1f364ca40c12e3e185f2fe181fbaba6
Rishi Malani <rmalani@marketo.com>
2017-12-15 11:51:47 -0800

    fix images

--- src/Images.js
+++ src/Images.js
@@ -7,22 +7,22 @@ export default class Images extends Component {
         <h3>Image gallery</h3>
         <h4>A Lovely Narwhal</h4>
         <a href="/">
-        <img src='Narwhal.jpg' alt="logo" />
+        <img src='https://raw.githubusercontent.com/rdubrock/narwhals/master/build/Narwhal.jpg' alt="logo" />
         </a>
         <h4>Another Lovely Narwhal</h4>
         <a href="/habitat">
-        <img src='narwhal_pod.jpg' alt="logo" />
+        <img src='https://raw.githubusercontent.com/rdubrock/narwhals/master/build/narwhal_pod.jpg' alt="logo" />
         </a>
         <h4>Narwhals are better than dolphins</h4>
         <a href="/behavior">
-        <img src='narwhal_tooth.jpg' alt="logo" />
+        <img src='https://raw.githubusercontent.com/rdubrock/narwhals/master/build/narwhal_tooth.jpg' alt="logo" />
         </a>
         <h4>Steve the narwhal</h4>
         <a href="/communications">
-        <img src='narwhals_underwater.jpg' alt="logo" />
+        <img src='https://raw.githubusercontent.com/rdubrock/narwhals/master/build/narwhals_underwater.jpg' alt="logo" />
         </a>
         <a href="/funfacts">
-          <img src='narwhals_funfacts.jpg' alt="logo" />
+          <img src='https://raw.githubusercontent.com/rdubrock/narwhals/master/build/narwhals_funfacts.jpg' alt="logo" />
         </a>
         <p>To submit your own narwhal picture, send us an email at narwahls@narwhals.narwhal</p>
       </div>
